2b7a839518745daaeba4149cd7bfc02a2fbf8b9f
Sannasi chottu <sannasichottu@gmail.com>
2021-11-15 21:19:11 +0530

    Movie list & color Game added

--- src/Counter.js
+++ src/Counter.js
@@ -1,3 +1,7 @@
+
+import IconButton from '@mui/material/IconButton';
+import Stack from '@mui/material/Stack';
+import DeleteIcon from '@mui/icons-material/Delete';
 import {useState} from "react";
 
 export function Counter() {
@@ -5,6 +9,12 @@ export function Counter() {
     const[dislike,setDislike]= useState(0);
     return(
       <div className="counter-countainer">
+       <IconButton aria-label="delete">
+  <DeleteIcon />
+</IconButton> 
+
+   {/* <Badge badgeContent ={like}  color = "primary"><MailIcon color ="action" /></Badge>
+     */ } 
         <button className="likes-dislikes" onClick={()=>{setLike(like+1);}}>👍 {like} </button>
         <button className="likes-dislikes" onClick={()=>{setDislike(dislike+1);}}>👎 {dislike} </button>
        
