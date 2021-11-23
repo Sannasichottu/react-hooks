fa8384b7fe81013f3e57f0c25662b562da4f723e
Sannasi chottu <sannasichottu@gmail.com>
2021-11-23 08:30:46 +0530

    Mockapi delete , edit connected

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, {useState , useEffect} from 'react';
 import {Counter} from "./Counter.js";
 //import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
@@ -30,69 +30,9 @@ import Paper from '@mui/material/Paper';
 
 function App() {
 
-  //Movie details
-  const INITIAL_MOVIES = [
-   {
-     name:"Master",
-     poster:"https://m.media-amazon.com/images/M/MV5BNmU1OTYzYzAtMDcyOS00MDI0LTg2ZmQtYTEyMDdmMmQ0MjY5XkEyXkFqcGdeQXVyOTk3NTc2MzE@._V1_FMjpg_UX1000_.jpg",
-     rating:7.8,
-     summary:"JD, an alcoholic professor, is enrolled to teach at a juvenile facility, unbeknownst to him. He soon clashes with a ruthless gangster, who uses the children as scapegoats for his crimes.",
-     trailer:"https://www.youtube.com/embed/1_iUFT3nWHk",
-   },
-   {
-    name:"Ratatouille",
-    poster:"https://images-na.ssl-images-amazon.com/images/S/pv-target-images/2b18044415b238c004e702aaf1dc693daf886d9dadf4b78e1b1bcc0b9df10f8c._RI_V_TTW_.jpg",
-    rating:8,
-    summary:"Remy, a rat, aspires to become a renowned French chef. However, he fails to realise that people despise rodents and will never enjoy a meal cooked by him.",
-    trailer:"https://www.youtube.com/embed/NgsQ8mVkN8w",
-  },
-  {
-    name:"Pokkiri",
-    poster:"https://qqcdnpictest.mxplay.com/pic/4e008988567d93c8edd379a374ec461f/en/2x3/320x480/test_pic1635764386337.webp",
-    rating:7.5,
-    summary:"Tamizh, a hardened criminal who does dirty jobs for money, falls in love with Shruthi, a charming girl, who hates his violent nature. His true purpose and identity are revealed after a turn of events.",
-    trailer:"https://www.youtube.com/embed/wj0GjTQshaw",
-  },
-  {
-    name:"Jai Bhim",
-    poster:"https://moviegalleri.net/wp-content/uploads/2021/07/Actor-Suriya-Jai-Bhim-Movie-First-Look-Poster-HD.jpg",
-    rating:9.6,
-    summary:"A pregnant woman from a primitive tribal community, searches desperately for her husband, who is missing from police custody. A High Court advocate rises in support to find her husband and seek justice for them.",
-    trailer:"https://www.youtube.com/embed/Gc6dEDnL8JA",
-  },
-  {
-    name:"The Conjuring",
-    poster:"https://m.media-amazon.com/images/M/MV5BOWRkOTYzZTQtMzQwNi00NDYwLTk4NjUtN2FjYTI4Y2UzM2RjXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_.jpg",
-    rating:7.5,
-    summary:"The Perron family moves into a farmhouse where they experience paranormal phenomena. They consult demonologists, Ed and Lorraine Warren, to help them get rid of the evil entity haunting them.",
-    trailer:"https://www.youtube.com/embed/tUc8BEuxAKY",
-  },
-  {
-    name:"Doctor",
-    poster:"https://images.cinemaexpress.com/uploads/user/imagelibrary/2021/3/11/original/Doctor.PNG",
-    rating:7.9,
-    summary:"An army surgeon assembles a team to help him track down his fiancee's kidnapped niece.",
-    trailer:"https://www.youtube.com/embed/oQiH_Iw0kDs",
-  },
-  {
-    name:"Ratsasan",
-    poster:"https://www.nettv4u.com/uploads/10-05-2018/raatchasan-movie-poster-.jpg",
-    rating:8.4,
-    summary:"Arun gives up on his dream of becoming a filmmaker and takes up the job of a police officer after his father's death. He then attempts to track down a psychotic killer who targets schoolgirls.",
-    trailer:"https://www.youtube.com/embed/GsrN7rNch9Y",
-  },
-  {
-    name:"Money Heist",
-    poster:"https://flxt.tmsimg.com/assets/p14100007_b_v9_au.jpg",
-    rating:8.2,
-    summary:"A criminal mastermind who goes by 'The Professor' has a plan to pull off the biggest heist in recorded history -- to print billions of euros in the Royal Mint of Spain. To help him carry out the ambitious plan, he recruits eight people with certain abilities and who have nothing to lose. The group of thieves take hostages to aid in their negotiations with the authorities, who strategize to come up with a way to capture The Professor. As more time elapses, the robbers prepare for a showdown with the police.",
-    trailer:"https://www.youtube.com/embed/DqZHAHwTwKY",
-  },
-  ];
- 
-  
+  //Movie details 
 
-  const [movies, setMovies]= useState(INITIAL_MOVIES);
+  const [movies, setMovies]= useState([]);
   const history = useHistory();
   const [mode,setMode]=useState("dark")
   ;
@@ -103,6 +43,16 @@ function App() {
     },
   });
  
+  
+
+  //useEffect(()=>{
+  //async function getMovies(){
+  //    const data = await fetch("https://619ba1512782760017445704.mockapi.io/movies");
+  //  const mvs = await data.json();
+  //     setMovies(mvs);
+  //  }getMovies();
+  //},[]);
+
   return (
     <ThemeProvider theme={theme} >
       <Paper elevation={3} style={{borderRadius:"0px", minHeight:"100vh"}} >
@@ -139,12 +89,12 @@ function App() {
           <Redirect to ="/movies" />
         </Route>
         <Route path="/movies/edit:id">
-          <Editmovie movies={movies} setMovies={setMovies} /></Route>
+          <Editmovie  /></Route>
         <Route path="/movies/:id">
         <MovieDetails movies={movies} />
         </Route>
         <Route path="/movies">
-        <MovieList movies={movies} setMovies={setMovies} />
+        <MovieList  />
         </Route>
         <Route path="/add-movies">
         <Editmovie movies={movies} setMovies={setMovies}/>
@@ -237,13 +187,20 @@ function App() {
   );  
   }
 
-
-
-
   function MovieDetails({movies}){
     const history=useHistory();
     const {id} = useParams();
-    const movie = movies[id];
+    //const movie = movies[id];
+    console.log("The id is", id);
+
+    const [movie, setMovie] = useState ({ }); 
+    useEffect(()=>{
+      fetch(`https://619ba1512782760017445704.mockapi.io/movies/${id}`,
+      {method:"GET",})
+      .then((data)=> data.json())
+      .then((mv)=> setMovie(mv));
+    },[]);
+
     const styles = {
       Color: movie.rating < 8 ? "crimson" : "green",
      fontWeight:"bold",};
@@ -287,12 +244,12 @@ function App() {
     const history =useHistory();
     const movie=movies[id];
     console.log(movie);
-   const [name, setName] = useState(movie.name);
-    const [poster, setPoster] = useState(movie.poster);
-    const [rating, setRating] = useState(movie.rating);
-    const [summary, setSummary] = useState(movie.summary);
-    const [trailer, setTrailer] = useState(movie.trailer);
-    
+   const [name, setName] = useState(" ");
+    const [poster, setPoster] = useState(" ");
+    const [rating, setRating] = useState(" ");
+    const [summary, setSummary] = useState(" ");
+    const [trailer, setTrailer] = useState(" ");
+  
     const editMovie = () => {
     
       const updatedMovie={
@@ -385,35 +342,46 @@ function App() {
 
 
 //MOvie-list
-function MovieList({movies, setMovies}) {
+function MovieList() {
+  const [movies, setMovies] = useState([]);
+
+  const getMovies =() =>{
+    fetch("https:619ba1512782760017445704.mockapi.io/movies/")
+    .then((data)=> data.json())
+    .then((mvs)=> setMovies(mvs));
+
+  }
+    
+  useEffect(getMovies,[]);
+
+  //After delete refresh
+  const deleteMovie = (id) => {
+    fetch(
+      `https://619ba1512782760017445704.mockapi.io/movies/${id}`,
+      {method:"DELETE"}
+      );
+  }
+
   const history = useHistory();
   return(
   <section className="movie-list">
 
-    {movies.map(({name,rating,summary,poster} ,index)=>(
-    <Movie 
-    name={name}
-    rating={rating}
-    summary={summary}
-    poster={poster}
-    id={index}
+    {movies.map(({name,rating,summary,poster,id} ,index)=>(
+      <Movie 
+        name={name}
+        rating={rating}
+        summary={summary}
+        poster={poster}
+        id={id}
 
-    deleteButton = {
-      <IconButton 
-      onClick={()=>{
-        console.log("Deleting..." ,index);
-         const deleteIdx = index;
-         const remainingMovies = movies.filter(
-           (mv,idx)=> idx !== deleteIdx
-         );
-         console.log("Remaining ", remainingMovies);
-         setMovies(remainingMovies);
-      }}
-      className="movie-show-button" 
-      aria-label="more-info" 
-      color="error">
-     <DeleteIcon />
-     </IconButton>
+        deleteButton = {
+        <IconButton 
+          onClick={()=>deleteMovie(id)}
+          className="movie-show-button" 
+          aria-label="more-info" 
+          color="error">
+        <DeleteIcon />
+        </IconButton>
     }
      
      
@@ -432,8 +400,7 @@ function MovieList({movies, setMovies}) {
   </ section> 
   );
   }
-
-
+  
 //Movie view details
 
 function Movie({name,rating,summary,poster,id, deleteButton,editButton}) {
